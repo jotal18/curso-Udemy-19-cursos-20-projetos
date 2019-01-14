b864c5847c5a3baf752a04d5c695010002d4f072
jotal18 <jotal18@hotmail.com>
2019-01-14 19:27:12 -0300

    Projeto Mata Mosquito Javascript

---
+++ JAVASCRIPT/projetoMataMosquito/jogo.js
@@ -0,0 +1,61 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+    // console.log(altura, largura)
+}
+ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica() {
+
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
+
+    var posicaox = Math.floor(Math.random() * largura) - 90
+    var posicaoy = Math.floor(Math.random() * altura) - 90
+
+    posicaox = posicaox < 0 ? 0 : posicaox
+    posicaoy = posicaoy < 0 ? 0 : posicaoy
+
+    console.log(posicaox, posicaoy)
+
+    var mosquito = document.createElement('img')
+
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
+    mosquito.style.left = posicaox + 'px'
+    mosquito.style.top = posicaoy + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+
+    document.body.appendChild(mosquito)
+
+    console.log(tamanhoAleatorio())
+    console.log(ladoAleatorio())
+}
+
+function tamanhoAleatorio() {
+    var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+    switch (classe) {
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
+}
